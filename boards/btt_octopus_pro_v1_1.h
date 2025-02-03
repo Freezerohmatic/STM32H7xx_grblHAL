99c85bcda0c3a9de0fafdb51c4e6005ebd25ba79
Freezerohmatic <michael.griffin@ieee.org>
2025-02-03 12:55:59 +1030

    Update btt_octopus_pro_v1_1.h
    Added copyright attribution to include myself

--- boards/btt_octopus_pro_v1_1.h
+++ boards/btt_octopus_pro_v1_1.h
@@ -5,6 +5,7 @@
 
   Copyright (c) 2024 Joe Corelli
   Copyright (c) 2024 Jon Escombe
+  Copyright (c) 2025 Michael Griffin
 
   Grbl is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
